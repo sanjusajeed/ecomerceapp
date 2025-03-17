0b2e1311552c6db86b1e323dc06dfea3e90577df
sanjusajeed <142177695+sanjusajeed@users.noreply.github.com>
2025-03-17 10:50:22 +0000

    Delete ProductService directory

--- ProductService/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();  // ✅ Add Controllers
-var app = builder.Build();
-
-app.MapControllers();  // ✅ Enable Controller Routing
-
-app.Run();
